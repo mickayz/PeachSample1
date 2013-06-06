337717b947de179c0242205ef6ba3cf930e1f160
mickayz <mick3x3@gmail.com>
2013-06-06 13:19:27 -0700

    sample peach pit and exploitable program

---
+++ filereader/filereader.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void usage(){
+  printf("Usage:\n");
+  printf("./secureapp FILENAME\n\n");
+  printf("Prints up to length bytes of ascii data\n");
+  printf("Securely designed to not print more data than exists :)\n\n");
+  printf("FILE STRUCTURE:\n\n");
+  printf("-------------------------------\n");
+  printf("|----0B---|----1B---|----2B---|\n");
+  printf("-------------------------------\n");
+  printf("|MAGIC 'M'|   Length(of Data)  |\n");
+  printf("-------------------------------\n");
+  printf("|        DATA (ascii)          |\n");
+  printf("|            ....             |\n");
+  printf("-------------------------------\n");
+
+  exit(1);
+}
+
+main(int argc, const char* argv[])
+{
+	FILE *file;
+	char *buffer;
+        char *tempbuf;
+        long fileLen;
+        char magic;
+        short maxlen;
+        unsigned int copyamount;
+
+        if (argc != 2){
+          usage();
+        }
+
+	file = fopen(argv[1], "rb");
+	if (!file)
+	{
+		fprintf(stderr, "can't open file %s", argv[1]);
+		exit(1);
+	}
+
+	fseek(file, 0, SEEK_END);
+	fileLen=ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	if (fileLen<3)
+	{
+		fprintf(stderr, "Invalid File Format!\n");
+                fclose(file);
+		exit(1);
+	}
+
+
+	tempbuf=(char *)malloc(fileLen);
+	if (!tempbuf)
+	{
+		fprintf(stderr, "Memory error!\n");
+                fclose(file);
+		exit(1);
+	}
+	fread(tempbuf, fileLen, 1, file);
+	fclose(file);
+
+
+        magic = (char)tempbuf[0];
+        memcpy(&maxlen, &(tempbuf[1]), 2);
+
+	if (magic!='M')
+	{
+		fprintf(stderr, "Invalid magic number!\n");
+		exit(1);
+	}
+
+
+
+	buffer=(char *)malloc(fileLen-2);
+	if (!buffer)
+	{
+		fprintf(stderr, "Memory error!\n");
+		exit(1);
+	}
+
+        // If maxlen defined in the file structure
+        // is less than the size of the data
+        // only copy up to maxlen
+        if (fileLen-3>maxlen){
+          copyamount = maxlen;
+        }
+        else{
+          copyamount = fileLen-3;
+        }
+
+        memcpy(buffer, &(tempbuf[3]), copyamount);
+        buffer[copyamount] = '\0';
+
+	printf ("value is %s \n", buffer);
+	free(buffer);
+        free(tempbuf);
+}
+
